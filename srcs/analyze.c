bfc62938058c5c185111b117d12c721f6becff94
lanou <lanou@ftping.ftping>
2026-08-13 18:55:44 +0200

    up

--- srcs/analyze.c
+++ srcs/analyze.c
@@ -1,7 +1,12 @@
 #include "ft_ping.h"
 
 int analyzer(t_ping *data, const unsigned char *buf, ssize_t len) {
+    struct iphdr    *ip_header;
+    
     if (len < sizeof(struct iphdr))
+        return (1);
+    ip_header = (struct iphdr *)buf;
+    if (len < ip_header->ihl * 4)
         return (1);
     return (0);
 }
