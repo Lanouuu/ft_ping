58cce74c7d326611ef2de32548cd5878fbf95aa9
lanou <lanou@Lanou.lanou.com>
2026-08-22 19:23:50 +0200

    up

--- srcs/socket.c
+++ srcs/socket.c
@@ -5,5 +5,19 @@ int init_socket(t_ping *data) {
     data->sockfd = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (data->sockfd == -1)
         return (dispatch_err(SOCK_ERR, NULL, errno));
+    if (data->verbose)
+    {
+        fprintf(
+            stderr,
+            "ping: sock4.fd: %d (socktype: SOCK_RAW), "
+            "hints.ai_family: AF_INET\n\n",
+            data->sockfd
+        );
+        fprintf(
+            stderr,
+            "ai->ai_family: AF_INET, ai->ai_canonname: '%s'\n",
+            data->hostname
+        );
+    }
     return (0);
 }
